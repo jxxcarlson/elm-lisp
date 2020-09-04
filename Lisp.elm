dcbe2015c8ba8f2486d0e59cd14b585e7b08cc50
James Carlson <jxxcarlson@gmail.com>
2020-09-04 00:37:18 -0400

    Add: eval list

--- Lisp.elm
+++ Lisp.elm
@@ -9,33 +9,19 @@ type Value
     = VStr String
     | VNum Int
     | VBool Bool
-    | VList (List String)
+    | VList (List Value)
     | Undefined
+    | ParseError
 
 
-eval : String -> Maybe Value
+eval : String -> Value
 eval str =
-    str |> Parse.toAST |> Maybe.map evalAst
+    case str |> Parse.toAST |> Maybe.map evalAst of
+        Just val ->
+            val
 
-
-unWrapVnum : Value -> Maybe Int
-unWrapVnum value =
-    case value of
-        VNum k ->
-            Just k
-
-        _ ->
-            Nothing
-
-
-wrapVnum : Maybe Int -> Value
-wrapVnum maybeInt =
-    case maybeInt of
         Nothing ->
-            Undefined
-
-        Just k ->
-            VNum k
+            ParseError
 
 
 evalAst : AST -> Value
@@ -56,10 +42,48 @@ evalAst ast =
                         |> wrapVnum
 
                 Just (Str "-") ->
-                    Maybe.map2 (-) (getNumArg 1 list) (getNumArg 2 list) |> wrapVnum
+                    if List.length list > 3 then
+                        -- Too many args
+                        Undefined
+
+                    else if List.length list < 2 then
+                        -- No args
+                        Undefined
+
+                    else if List.length list == 2 then
+                        -- Unary minus
+                        Maybe.map (\x -> -x) (getNumArg 1 list) |> wrapVnum
+
+                    else
+                        -- Binary minus
+                        Maybe.map2 (-) (getNumArg 1 list) (getNumArg 2 list) |> wrapVnum
 
                 _ ->
-                    Undefined
+                    List.map evalAst list |> VList
+
+
+
+-- HELPERS
+
+
+unWrapVnum : Value -> Maybe Int
+unWrapVnum value =
+    case value of
+        VNum k ->
+            Just k
+
+        _ ->
+            Nothing
+
+
+wrapVnum : Maybe Int -> Value
+wrapVnum maybeInt =
+    case maybeInt of
+        Nothing ->
+            Undefined
+
+        Just k ->
+            VNum k
 
 
 getNumArg : Int -> List AST -> Maybe Int
